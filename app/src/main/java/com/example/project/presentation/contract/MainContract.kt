2f5fe002bce88e956bd5b2cc76ab56ac0b40bbc6
Adriano Saviolli <saviolli@ciandt.com>
2020-07-22 15:02:25 -0300

    Test

--- app/src/main/java/com/example/project/presentation/contract/MainContract.kt
+++ app/src/main/java/com/example/project/presentation/contract/MainContract.kt
@@ -4,15 +4,12 @@ import android.widget.EditText
 import com.example.project.presentation.activity.MainActivity
 
 interface MainContract {
-
     interface View {
-
         fun showResult(result: String)
 
         fun showError(error: String)
     }
     interface Presenter {
-
         fun calculate(view: MainActivity, operation: String, number1: EditText, number2: EditText)
     }
 }
